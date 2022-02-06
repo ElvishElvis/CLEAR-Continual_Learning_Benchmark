0236ea16048f5ffd87386a048d8c690aa2d24558
ElvishElvis <elvishelvis6@gmail.com>
2022-02-06 13:42:05 -0500

    restructure code

--- temp.py
+++
@@ -1,8 +0,0 @@
-# import argparse
-# argparser = argparse.ArgumentParser()
-# argparser.add_argument("--method",default='Naive GDumb')
-# args = argparser.parse_args()
-# print(args.method.split())
-filename='sdfsdfsdfsd'
-with open('out.txt', 'w') as f:
-    print('Filename:', filename, file=f)
