d72c0486d04d3328d6ed08830afee8298d6abc15
Elvis Shi <40554410+ElvishElvis@users.noreply.github.com>
2022-06-24 10:33:58 -0500

    Add files via upload

---
+++ download.sh
@@ -0,0 +1,4 @@
+# clear10
+wget https://clear-challenge.s3.us-east-2.amazonaws.com/clear10-public.zip
+# clear100
+wget https://clear-challenge.s3.us-east-2.amazonaws.com/clear100-workshop-avalanche.zip
